46a48f828c2eb739dea37311e114bc07194ae9ec
Ann Kidder <annkidder@Anns-Air-2.home>
2017-06-22 11:14:18 -0400

    cleaned up comments and added csv module

--- startercode/problem1.py
+++ startercode/problem1.py
@@ -1,26 +1,9 @@
 '''
 Perceptron Learning Algorithm
-Input: CSV set of linearly seperable datapoints
-Output: CSV set of weights for each datapoint
-
-#STEP 1: initialize weights to 0 
-weight = [0, 0]
-t = 1
-
-for each example, predict positive if weight*x >0
-if there's an error:
-	if the mistake is on a positive w of t+1 = wt + x
-	if the mistake is on a negative w of t+1 = wt - x
-	t = t + 1
-
-activation = sum(weight_i * x_i) + bias # (sum of each weight * feature) + bias
-
-# repeat until convergence (until all weights are >= 0)
+Input: CSV set of linearly seperable datapoints (x, y, classification)
+Output: CSV set of weights for each datapoint (w_1, w_2, bias)
 '''
-import numpy as np
-import random
-import os, subprocess
-import matplotlib.pyplot as plt
+
 import itertools
 import pdb
 import sys
@@ -32,16 +15,15 @@ class Perceptron(object):
         self.input = inp
         self.output = out
         self.features = []
-        self.weights = [-5,-2,39]
+        self.weights = [0,0,0]
         self.prev_w = None
 
     def set_features(self):
-        self.features = [[map(int, i.split(',')) for i in (item.strip() 
-                                        for item in line.rstrip('\n').split('\n'))][0] 
-                                        for line in open(self.input)]
+        self.features = [map(int,i) for i in csv.reader(open('input1.csv'))]
     
     def predict(self):
     	out = open(self.output, 'a+')
+    	# repeat until convergence
         while self.prev_w != self.weights:
             self.update_weights()
             format = [str(self.weights[0]), str(self.weights[1]), str(self.weights[2])]
@@ -49,7 +31,6 @@ class Perceptron(object):
         out.close()
 
     def update_weights(self):
-    	#pdb.set_trace()
         cycle = itertools.cycle(self.features)
         self.prev_w = [i for i in self.weights]
         for i in range(len(self.features)):
@@ -62,12 +43,10 @@ class Perceptron(object):
 	
     def error(self, row):
         activation = 0
-        pdb.set_trace()
+        #pdb.set_trace()
         for i in range(len(row) - 1):
-            activation += self.weights[i] * row[i] #w_1, w_2, w_0
+            activation += self.weights[i] * row[i] 
         return 1 if activation > -self.weights[2] else -1
-
-
 
 def main(argv):
     try:
